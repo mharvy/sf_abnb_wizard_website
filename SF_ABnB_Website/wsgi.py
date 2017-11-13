f05792b2867b4d3262bb6df8fbcbe474f6faf9cd
mharvy <32440398+mharvy@users.noreply.github.com>
2017-11-13 03:26:49 -0600

    Uploading main app, leaving out settings.py

---
+++ SF_ABnB_Website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SF_ABnB_Website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SF_ABnB_Website.settings")
+
+application = get_wsgi_application()
